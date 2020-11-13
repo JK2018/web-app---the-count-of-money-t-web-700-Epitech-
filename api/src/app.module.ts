96fd773ee55ad5e9f400c633a8178a79334f8d14
LucasDbs <lucas.duboisse@epitech.eu>
2020-11-13 17:28:14 +0100

    [+] User module / User CRUD / Crypto module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
+import { UserModule } from './user/user.module';
+import { CryptoModule } from './crypto/crypto.module';
 
 @Module({
   imports: [
-      TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+      TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+      UserModule,
+      CryptoModule
   ],
   controllers: [AppController],
   providers: [AppService],
