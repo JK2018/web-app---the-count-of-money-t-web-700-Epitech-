7c005e5abc5500aa0133f62c989ff93d59fd66f2
JK2018 <khalifa_jonathan@hotmail.com>
2020-11-15 11:34:30 +0100

    fetched crypto api data

--- front/src/component/Crypto.js
+++ front/src/component/Crypto.js
@@ -8,14 +8,14 @@ const Crypto = (props) => {
             <li>
                     <div className="lidiv">
                         <p className="pml"><FontAwesomeIcon icon={faStar}/></p>
-                        <p className="pxl">{props.coin}</p>
+                        <p className="pml">{props.rank}</p>
+                        <img className="iconimg" src={props.img} alt=""/><p className="pxl coinName">{props.coin}</p>
                         <p className="pml">{props.tag}</p>
                         <p className="pxl">{props.price}</p>
-                        <p className="pml">{props.onehour}</p>
-                        <p className="pml">{props.oneday}</p>
-                        <p className="pml">{props.oneweek}</p>
-                        <p className="pxl">{props.dayvol}</p>
-                        <p className="pxl">{props.mcap}</p>
+                        <p className="pml">{props.oneday}%</p>
+                        <p className="pml4">{props.onedaycurr} $</p>
+                        <p className="pxl">{props.dayvol}K</p>
+                        <p className="pxl-4">{props.mcap}M</p>
                     </div>
                 </li>
         </div>
--- front/src/component/CryptoList.js
+++ front/src/component/CryptoList.js
@@ -1,34 +1,60 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Fragment } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import Crypto from './Crypto';
+import axios from 'axios';
+
 
 const CryptoList = () => {
+    //const elements = {};
+    //const state = { count: 0 };
+    const apiUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C%2024h%2C%207d";
 
-
-
-
-
+    const [data, setData] = useState([]);
+ 
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await axios(apiUrl,);
+            setData(result.data);
+        }
+        fetchData();
+    }, []);
+console.log(data);
 
     return (
-        <div className="test">
-            <h1>test INSIDE LIST</h1>
+        <div>
+            
             <ul>
                 <li>
                     <div className="lidiv">
                         <p className="pml"><FontAwesomeIcon icon={faStar}/></p>
-                        <p className="pxl">Coin</p>
+                        <p className="pml">Rank</p>
+                        <p className="iconimg"> </p>
+                        <p className="pxl coinName">Coin</p>
                         <p className="pml">Tag</p>
                         <p className="pxl">Price</p>
-                        <p className="pml">1h</p>
-                        <p className="pml">24h</p>
-                        <p className="pml">7d</p>
-                        <p className="pxl">24h Volume</p>
-                        <p className="pxl">Market Cap</p>
+                        <p className="pml">24h %</p>
+                        <p className="pml4">24h $</p>
+                        <p className="pxl">Total Volume</p>
+                        <p className="pxl-4">Market Cap</p>
                     </div>
                 </li>
-                <Crypto coin="bitcoin" tag="BTC" price="13000" onehour="0.3%" oneday="0.45%" oneweek="-0.1%" dayvol="1243K" mcap="1231M"></Crypto>
-                
+                {data.map(item => (
+                <Fragment key={item.market_cap_rank}>
+                    <Crypto img={item.image} 
+                    coin={item.name} 
+                    tag={item.symbol} 
+                    price={item.current_price} 
+                    rank={parseInt(item.market_cap_rank)} 
+                    oneday={item.price_change_percentage_24h.toString().slice(0, -3)} 
+                    onedaycurr={parseInt(item.price_change_24h).toFixed(2)} 
+                    dayvol={(parseInt(item.total_volume)/1000).toFixed(2)} 
+                    mcap={(parseInt(item.market_cap)/1000000).toFixed(2)}>
+                    
+                    </Crypto>
+                </Fragment>
+            ))}
             </ul>
         </div>
     )
